5a0dc1cfc762bbcacfd583d1a1be1030fe22327c
salvoab <salvatore.abate85@gmail.com>
2020-12-10 17:51:32 +0100

    Milestone 2 creando con filter e map 3 array distinti
    I tre array sono rispettivamente per icone di animali, vegetali e persone

--- icons.js
+++ icons.js
@@ -16,8 +16,8 @@ $(document).ready(function () {
     new Icon('dog', 'fa', 'animal', 'fas'),
     new Icon('dove', 'fa', 'animal', 'fas'),
     new Icon('carrot', 'fa', 'vegetable', 'fas'),
-    new Icon('pepper-hot', 'fa', 'animal', 'fas'),
-    new Icon('apple-alt', 'fa', 'animal', 'fas'),
+    new Icon('pepper-hot', 'fa', 'vegetable', 'fas'),
+    new Icon('apple-alt', 'fa', 'vegetable', 'fas'),
     new Icon('child', 'fa', 'people', 'fas'),
     new Icon('female', 'fa', 'people', 'fas'),
     new Icon('male', 'fa', 'people', 'fas')
@@ -25,15 +25,37 @@ $(document).ready(function () {
   // Selezioniamo il container icons
   const iconsContainer = $('.icons');
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
-  showIcons(iconsContainer, arrayIcons);
+  //showIcons(iconsContainer, arrayIcons); //Solo per milestone 1 per gli altri milestone viene commentato
   
   ///////////////
   // Milestone 2
-  // definiamo dei colori per le icone (blue, orange, purple)
-
-  //aggiungiamo dei colori usando una funzione
+  // definiamo dei colori per le icone (blue, orange, purple) 
+  //----> blue per animal | orange per vegetable | purple per people
+  const blue = '#0000FF';
+  const orange = '#FFA500';
+  const purple = '#800080';
+  
+  //aggiungiamo dei colori (usando una funzione)
+  const animalIcons = arrayIcons.filter(icon => icon.type == 'animal').map(animal => {
+    const {name, prefix, type, family} = animal;
+    return {name, prefix, type, family, color: blue};
+  });
+  //animalIcons.forEach(animal => animal.color = blue);
+  const vegetableIcons = arrayIcons.filter(icon => icon.type == 'vegetable').map(vegetable => {
+    const {name, prefix, type, family} = vegetable;
+    return {name, prefix, type, family, color: orange};
+  });
+  //vegetableIcons.forEach(animal => animal.color = orange);
+  const peopleIcons = arrayIcons.filter(icon => icon.type == 'people').map(person => {
+    const {name, prefix, type, family} = person;
+    return {name, prefix, type, family, color: purple};
+  });
+  //peopleIcons.forEach(animal => animal.color = purple);
   
   //inseriamo le icone colorate nel container
+  showIcons(iconsContainer, animalIcons);
+  showIcons(iconsContainer, vegetableIcons);
+  showIcons(iconsContainer, peopleIcons);
 
   ///////////////
   // Milestone 3
@@ -57,19 +79,10 @@ function showIcons(selector, icons){
   icons.forEach(icon => {
     const markup = `
     <div>
-      <i class="${icon.family} ${icon.prefix}-${icon.name}"></i>
+      <i class="${icon.family} ${icon.prefix}-${icon.name}" style="color:${icon.color}"></i>
       <div class="title">${icon.name.toUpperCase()}</div>
     </div>
     `;
     selector.append(markup);
-    console.log(markup);
   });
 }
-
-///////////////
-// Milestone 2
-// funzione per assegnare un colore ad un icona
-// tip: possiamo invocare qui dentro un'altra funzione (2) oppure usare map 
-// funzione per ottenere da un array
-// tip: possiamo usarla nella funzione precedente oppure usare map senza creare questa funzione.
-
