aa62347d1cb57baba51dfef09e7d5e2779c27d84
salvoab <salvatore.abate85@gmail.com>
2020-12-10 16:10:11 +0100

    Cartelle e file forniti per l'esercizio
    Il file icons.js contiene le istruzioni per le tre milestone dell'esercizio

---
+++ icons.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  ///////////////
+  // Milestone 1
+  // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family 
+
+  // Selezioniamo il container icons
+
+  //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
+
+  ///////////////
+  // Milestone 2
+  // definiamo dei colori per le icone (blue, orange, purple)
+
+  //aggiungiamo dei colori usando una funzione
+  
+  //inseriamo le icone colorate nel container
+
+  ///////////////
+  // Milestone 3
+  //estrapoliamo i tipi di icone
+
+  //aggiungiamo i tipi alla select
+
+  //al change mostriamo solo le icone filtrate
+  //mostriamo come passare un parametro a change e contemporaneamente destrutturiamo
+  
+});
+
+
+/* ---- FUNCTIONS ----*/
+///////////////
+// Milestone 1
+// Creiamo una funzione per iterare nell'array e che appenda al container le icone.
+// tip: possiamo usare un ciclo foreach qui e destrutturare gli le proprieta degli elementi di un array
+// tip: il template literal ci puo aiutare con il markup
+
+///////////////
+// Milestone 2
+// funzione per assegnare un colore ad un icona
+// tip: possiamo invocare qui dentro un'altra funzione (2) oppure usare map 
+// funzione per ottenere da un array
+// tip: possiamo usarla nella funzione precedente oppure usare map senza creare questa funzione.
+
